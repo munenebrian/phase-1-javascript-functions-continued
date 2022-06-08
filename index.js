506506fadad11ae803bf5199e75b370a7d0db0e6
munenebrian <njerubrianmunene@gmail.com>
2022-06-08 20:21:14 +0300

    commit tree e715fba30be9c1cedb519702f1e4ee32e380f5b1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun = `roller-skate`) {
+    return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun(`bathe my dog`);
+const mondayWork = function(ToDo = `go to the office`) {
+    return `This Monday, I will ${ToDo}.`
+}
+mondayWork(`work from home`);
+
+function wrapAdjective(star = "*") {
+    function innerFunction(word = "special") {
+        return `You are ${star}${word}${star}!`
+    }
+    return innerFunction;
+}
+const final = wrapAdjective("*")("a dedicated programmer");
